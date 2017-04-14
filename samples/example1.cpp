0b84023a4f5918291ab7d016df5cf66cfb48249a
Dibyendu Majumdar <noemail>
2017-04-15 00:38:16 +0100

    added int32 multiply example/test

--- samples/example1.cpp
+++ samples/example1.cpp
@@ -69,7 +69,7 @@ int add(NJXContextRef jit) {
   auto param2 = NJX_insert_parameter(builder); /* arg2 */
   auto x = NJX_q2i(builder, param1);           /* x = (int) arg1 */
   auto y = NJX_q2i(builder, param2);           /* y = (int) arg2 */
-  auto result = NJX_addi(builder, x, y);       /* result = x = y */
+  auto result = NJX_addi(builder, x, y);       /* result = x + y */
   auto ret = NJX_reti(builder, result);        /* return result */
 
   functype f = (functype)NJX_finalize(builder);
@@ -92,7 +92,7 @@ int calladd(NJXContextRef jit) {
   auto param2 = NJX_insert_parameter(builder); /* arg2 */
   auto x = NJX_q2i(builder, param1);           /* x = (int) arg1 */
   auto y = NJX_q2i(builder, param2);           /* y = (int) arg2 */
-  auto result = NJX_addi(builder, x, y);       /* result = x = y */
+  auto result = NJX_addi(builder, x, y);       /* result = x + y */
   auto ret = NJX_reti(builder, result);        /* return result */
   adderfunc fadd = (adderfunc)NJX_finalize(builder);
   NJX_destroy_function_builder(builder);
@@ -114,6 +114,32 @@ int calladd(NJXContextRef jit) {
   return 1;
 }
 
+/**
+* Tests a simple add function that takes two int values and returns their sum
+*/
+int mult(NJXContextRef jit) {
+	const char *name = "mult";
+	typedef int (*functype)(NJXParamType, NJXParamType);
+
+	// Create a function builder
+	NJXFunctionBuilderRef builder = NJX_create_function_builder(jit, name, true);
+
+	auto param1 = NJX_insert_parameter(builder); /* arg1 */
+	auto param2 = NJX_insert_parameter(builder); /* arg2 */
+	auto x = NJX_q2i(builder, param1);           /* x = (int) arg1 */
+	auto y = NJX_q2i(builder, param2);           /* y = (int) arg2 */
+	auto result = NJX_muli(builder, x, y);       /* result = x * y */
+	auto ret = NJX_reti(builder, result);        /* return result */
+
+	functype f = (functype)NJX_finalize(builder);
+
+	NJX_destroy_function_builder(builder);
+
+	if (f != nullptr)
+		return f(100, 200) == 20000 ? 0 : 1;
+	return 1;
+}
+
 int main(int argc, const char *argv[]) {
 
   NJXContextRef jit = NJX_create_context(true);
@@ -122,6 +148,7 @@ int main(int argc, const char *argv[]) {
   rc += simplest(jit);
   rc += add2(jit);
   rc += add(jit);
+  rc += mult(jit);
   rc += calladd(jit);
 
   NJX_destroy_context(jit);
